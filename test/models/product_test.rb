c374fc3b5fa5e34695ff070f3ee2d90b4162fd14
Rafael Williams <rafael.williams@gmail.com>
2016-09-05 09:58:15 -0500

    Validation!

--- test/models/product_test.rb
+++ test/models/product_test.rb
@@ -1,6 +1,7 @@
 require 'test_helper'
 
 class ProductTest < ActiveSupport::TestCase
+  fixtures :products
   # test "the truth" do
   #   assert true
   # end
@@ -40,5 +41,16 @@ class ProductTest < ActiveSupport::TestCase
 
   end
 
+  test "product is not valid without a unique title" do
+    product = Product.new(title: products(:ruby).title, description: "yyy", price: 1, image_url: "fred.gif")
+    assert product.invalid?
+    assert_equal ["has already been taken"], product.errors[:title] 
+  end
+
+  test "product is not valid without a unique title - i18n" do 
+    product = Product.new(title: products(:ruby).title, description: "yyy", price: 1, image_url: "fred.gif")
+    assert product.invalid?
+    assert_equal [I18n.translate('errors.messages.taken')], product.errors[:title]
+  end
 
 end
