65be39fa73d9586b8badeea1c6aa1b946f2ce40b
Rafael Williams <rafael.williams@gmail.com>
2016-09-07 14:03:53 -0500

    smart card

--- app/controllers/concerns/current_cart.rb
+++ app/controllers/concerns/current_cart.rb
@@ -1,14 +1,12 @@
-Module CurrentCart
- extend ActiveSupport::Concern
+module CurrentCart
+  extend ActiveSupport::Concern
 
- private
+  private
 
- def set_cart 
-   @cart.find(session[:cart_id])
- rescue ActiveRecord::RecordNotFound
- @card = Card.create
- session[:cart_id] = @cart.id
-   
- end
-
+  def set_cart
+    @cart = Cart.find(session[:cart_id])
+  rescue ActiveRecord::RecordNotFound
+    @cart = Cart.create
+    session[:cart_id] = @cart.id
+  end
 end
